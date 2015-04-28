5b55435ff457010831f18bb4be933a244b2ca375
tanelih <taneli.hartikainen@jamk.fi>
2015-04-28 13:33:39 +0300

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# jarmo-influxdb-reporter
-InfluxDB repoter for Jarmo.
+# Jarmo InfluxDB Reporter
+Reporter module for [Jarmo](https://github.com/N4SJAMK/jarmo), pushes data to
+InfluxDB. Note that this module is for InfluxDB 0.9 and up!
+
+## Configuration
+Configuration can be passed in under the `influxdb` key. Like so:
+
+```javascript
+export default {
+	reporters: [
+		'../backends/influxdb'
+	],
+	influxdb: {
+		host:     'localhost',  // Host of the InfluxDB server.
+		port:     8000,         // Port of the InfluxDB server.
+		database: 'test',       // Database name.
+		username: 'test',       // Username for basic authentication.
+		password: 'test'        // Password for basic authentication.
+	}
+}
+```
+
+## Data Format
+Since Jarmo expects JSON, the received data is actually a JSON object. In order
+for this reporter to work, the data should be in the following format:
+
+```javascript
+{
+	name: 'response',  // InfluxDB 'series' name.
+	tags: {
+		// Tags for this 'data point'.
+		host: 'abc_01'
+	},
+	fields: {
+		// Fields for this 'data point'.
+		response_time:   26,
+		response_status: 300
+	}
+}
+```
+You can also add a `timestamp` field to the payload, which will overwrite the
+timestamp set by Jarmo.
---
+++ index.js
@@ -0,0 +1,103 @@
+import http from 'http';
+
+/**
+ * InfluxDB reporter.
+ */
+export default {
+	/**
+	 * Each 'reporter' should have a name, since this determines the config
+	 * that is passed to the 'initialize'.
+	 */
+	name: 'influxdb',
+
+	/**
+	 * Backend initialization should be done in this method, this means that if
+	 * the module needs to create a connection to somewhere like a database, we
+	 * do it here.
+	 *
+	 * @param {object} config  The configuration object for this reporter. Note
+	 *                         that 'this.name' will be used as a 'key' to pick
+	 *                         out the configuration for this reporter.
+	 */
+	initialize(config = {}) {
+		this.config = config;
+	},
+
+	/**
+	 * Flush given data to InfluxDB.
+	 *
+	 * @param {object[]} points  Upon each flush cycle, depending on the server
+	 *                           configuration, we receive a set of data which
+	 *                           we must 'flush' somewhere.
+	 */
+	flush(points) {
+		return new Promise((resolve, reject) => {
+			if(points.length === 0) return resolve();
+
+			let payload = {
+				points:   points.map(format),
+				database: this.config.database
+			}
+			return post(JSON.stringify(payload), this.config)
+				.then(resolve, reject);
+		});
+	}
+}
+
+/**
+ * Send a POST request to InfluxDB.
+ *
+ * @param {string} payload  Stringified JSON payload.
+ * @param {object} config   The configuration for this reporter.
+ *
+ * @return {Promise} Promise resolved when the data has been sent to InfluxDB.
+ */
+function post(payload, config) {
+	let options = {
+		host: config.host,
+		port: config.port,
+
+		// We use 'basic' authentication for our request.
+		auth: `${config.username}:${config.password}`,
+
+		path:   '/write',
+		method: 'POST',
+
+		headers: {
+			'Content-Type':   'application/json',
+			'Content-Length': payload.length
+		}
+	}
+	return new Promise((resolve, reject) => {
+		// Create the request based on the options we defined above. If the
+		// response is OK everything went better than expected.
+		let request = http.request(options, (res) => {
+			if(res.statusCode === 200) {
+				return resolve();
+			}
+			return reject(new Error(`Expected 200. Got ${res.statusCode}.`));
+		});
+
+		// Add a listener for 'error' events, so that they get caught.
+		request.once('error', reject);
+
+		// Write out our data and close the request to signify no that no more
+		// data will be sent.
+		request.write(payload);
+		request.end();
+	});
+}
+
+/**
+ * Format given points so that they will match the format used by InfluxDB.
+ */
+function format(point) {
+	return {
+		name:   point.data.name,
+		tags:   point.data.tags,
+		fields: point.data.fields,
+		// If the data itself contains a timestamp, we use that instead of the
+		// default timestamp added when the message is received here.
+		timestamp: point.data.timestamp || point.timestamp
+	}
+}
